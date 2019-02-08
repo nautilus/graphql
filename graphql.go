6bb377f3952a34d399766e71f13eeb8e6cc30889
Alec Aivazis <alec@aivazis.com>
2019-02-08 11:32:07 -0800

    fixed error reporting in LoadSchema

--- graphql.go
+++ graphql.go
@@ -7,7 +7,13 @@ import (
 
 // LoadSchema takes an SDL string and returns the parsed version
 func LoadSchema(typedef string) (*ast.Schema, error) {
-	return gqlparser.LoadSchema(&ast.Source{
+	schema, err := gqlparser.LoadSchema(&ast.Source{
 		Input: typedef,
 	})
+
+	// vektah/gqlparser returns non-nil err all the time
+	if schema == nil {
+		return nil, err
+	}
+	return schema, nil
 }
