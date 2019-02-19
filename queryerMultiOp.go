f31c46cb3731fe2132117007c4ace0950034339d
Alec Aivazis <alec@aivazis.com>
2019-02-19 13:47:43 -0800

    comments

--- queryerMultiOp.go
+++ queryerMultiOp.go
@@ -12,23 +12,33 @@ type MultiOpQueryer struct {
 	MaxBatchSize  int
 	BatchInterval time.Duration
 	URL           string
-	Client        http.Client
 
+	client      *http.Client
 	middlewares []NetworkMiddleware
+}
+
+// NewMultiOpQueryer returns a MultiOpQueryer with the provided paramters
+func NewMultiOpQueryer(url string, interval time.Duration, maxBatchSize int) *MultiOpQueryer {
+	return &MultiOpQueryer{
+		MaxBatchSize:  maxBatchSize,
+		BatchInterval: interval,
+		URL:           url,
+	}
 }
 
 // WithMiddlewares lets the user assign middlewares to the queryer
 func (q *MultiOpQueryer) WithMiddlewares(mwares []NetworkMiddleware) Queryer {
-	return &MultiOpQueryer{
-		URL:           q.URL,
-		Client:        q.Client,
-		BatchInterval: q.BatchInterval,
-		MaxBatchSize:  q.MaxBatchSize,
-		middlewares:   mwares,
-	}
+	q.middlewares = mwares
+	return q
 }
 
-// Query bundles queries that happen within the given interval into a single network request
+// WithHTTPClient lets the user configure the client to use when making network requests
+func (q *MultiOpQueryer) WithHTTPClient(client *http.Client) Queryer {
+	q.client = client
+	return q
+}
+
+// Query bundles queries that happen Swithin the given interval into a single network request
 // whose body is a list of the operation payload.
 func (q *MultiOpQueryer) Query(ctx context.Context, input *QueryInput, receiver interface{}) error {
 	return nil
